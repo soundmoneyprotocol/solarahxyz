70eea791a6740ddd055dfb2db7340fdec6ca77a7
soundmoneyprotocol <casmirconsults@gmail.com>
2024-10-01 12:42:48 -0500

    updated ./pages/authors/[name].js to /solarah.js and commented out for now to deploy to vercel TODO: create routes for /authors and slug for /lib/ getPosts /post

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,43 +1,42 @@
-// pages/posts/[slug].js
+// // pages/posts/[slug].js
 
-import { getSinglePost, getPosts } from '../lib/posts';
+// import { getSinglePost, getPosts } from '../lib/posts';
 
-// PostPage page component
-const PostPage = (props) => {
-  // Render post title and content in the page from props
-  return (
-    <div>
-      <h1>{props.post.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: props.post.html }} />
-    </div>
-  )
-}
+// // PostPage page component
+// const PostPage = (props) => {
+//   // Render post title and content in the page from props
+//   return (
+//     <div>
+//       <h1>{props.post.title}</h1>
+//       <div dangerouslySetInnerHTML={{ __html: props.post.html }} />
+//     </div>
+//   )
+// }
 
-export async function getStaticPaths() {
-  const posts = await getPosts()
+// export async function getStaticPaths() {
+//   const posts = await getPosts()
 
-  // Get the paths we want to create based on posts
-  const paths = posts.map((post) => ({
-    params: { slug: post.slug },
-  }))
+//   // Get the paths we want to create based on posts
+//   const paths = posts.map((post) => ({
+//     params: { slug: post.slug },
+//   }))
 
-  // { fallback: false } means posts not found should 404.
-  return { paths, fallback: false }
-}
+//   // { fallback: false } means posts not found should 404.
+//   return { paths, fallback: false }
+// }
 
+// // Pass the page slug over to the "getSinglePost" function
+// // In turn passing it to the posts.read() to query the Ghost Content API
+// export async function getStaticProps(context) {
+//   const post = await getSinglePost(context.params.slug)
 
-// Pass the page slug over to the "getSinglePost" function
-// In turn passing it to the posts.read() to query the Ghost Content API
-export async function getStaticProps(context) {
-  const post = await getSinglePost(context.params.slug)
+//   if (!post) {
+//     return {
+//       notFound: true,
+//     }
+//   }
 
-  if (!post) {
-    return {
-      notFound: true,
-    }
-  }
-
-  return {
-    props: { post }
-  }
-}
+//   return {
+//     props: { post }
+//   }
+// }
