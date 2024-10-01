60e70555fa7036c2c66df559a9929cd7cb2a5887
soundmoneyprotocol <casmirconsults@gmail.com>
2024-10-01 12:38:57 -0500

    updated ./pages/authors/[name].js to /solarah.js and commented out for now to deploy to vercel TODO: create routes for /authors

--- pages/authors/solarah.js
+++ pages/authors/solarah.js
@@ -1,34 +1,34 @@
-import { getAuthor, getAllAuthors } from '../../api/authors';
+// import { getAuthor, getAllAuthors } from '../../api/authors';
 
-const AuthorPage = (props) => {
-  return (
-    <div>
-      <h1>{props.author.name}</h1>
-      <div dangerouslySetInnerHTML={{ __html: props.author.bio }} />
-    </div>
-  );
-};
+// const AuthorPage = (props) => {
+//   return (
+//     <div>
+//       <h1>{props.author.name}</h1>
+//       <div dangerouslySetInnerHTML={{ __html: props.author.bio }} />
+//     </div>
+//   );
+// };
 
-export async function getStaticPaths() {
-  const authors = await getAllAuthors();
-  const paths = authors.map((author) => ({
-    params: { name: author.name },
-  }));
+// export async function getStaticPaths() {
+//   const authors = await getAllAuthors();
+//   const paths = authors.map((author) => ({
+//     params: { name: author.name },
+//   }));
 
-  return { paths, fallback: false };
-}
+//   return { paths, fallback: false };
+// }
 
-export async function getStaticProps(context) {
-  const author = await getAuthor(context.params.name);
+// export async function getStaticProps(context) {
+//   const author = await getAuthor(context.params.name);
 
-  if (!author) {
-    return {
-      notFound: true,
-    };
-  }
+//   if (!author) {
+//     return {
+//       notFound: true,
+//     };
+//   }
 
-  return {
-    props: { author },
-  };
-}
-export default AuthorPage;
+//   return {
+//     props: { author },
+//   };
+// }
+// export default AuthorPage;
