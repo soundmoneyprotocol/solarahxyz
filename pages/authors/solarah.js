99d82f46636083a22f1d3cf4bc98afe11c7394a0
soundmoneyprotocol <casmirconsults@gmail.com>
2024-10-01 12:36:28 -0500

    updated ./pages/authors/[name].js to /solarah.js

--- pages/authors/solarah.js
+++ pages/authors/solarah.js
@@ -6,30 +6,29 @@ const AuthorPage = (props) => {
       <h1>{props.author.name}</h1>
       <div dangerouslySetInnerHTML={{ __html: props.author.bio }} />
     </div>
-  )
-}
+  );
+};
 
 export async function getStaticPaths() {
-  const authors = await getAllAuthors()
+  const authors = await getAllAuthors();
   const paths = authors.map((author) => ({
     params: { name: author.name },
-  }))
+  }));
 
-  return { paths, fallback: false }
+  return { paths, fallback: false };
 }
 
-
 export async function getStaticProps(context) {
-  const author = await getAuthor(context.params.name)
+  const author = await getAuthor(context.params.name);
 
   if (!author) {
     return {
       notFound: true,
-    }
+    };
   }
 
   return {
-    props: { author }
-  }
+    props: { author },
+  };
 }
 export default AuthorPage;
