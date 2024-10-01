7f5c8f8a85a5a02c7b2be0d05d5454f19872e200
soundmoneyprotocol <casmirconsults@gmail.com>
2024-10-01 12:32:27 -0500

    added https://pay.solarah.xyz to homepage

--- pages/index.js
+++ pages/index.js
@@ -121,7 +121,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://www.paypal.com/paypalme/tanyatarot19"
+          href="https://pay.solarah.xyz/"
           target="_blank"
           rel="noopener noreferrer"
         >
