c5e4ec9949e6ba19ffaadff148482847c8bbd868
soundmoneyprotocol <casmirconsults@gmail.com>
2024-10-04 11:15:08 -0500

    updated /profiles/solarah with a button for solarah.xyz

--- pages/index.js
+++ pages/index.js
@@ -166,7 +166,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to solarahspeaks.info to view services→
+          Go to solarah.info to view services→
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
